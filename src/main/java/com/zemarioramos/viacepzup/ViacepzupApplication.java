70124eb7a1859b1c9fce70e4095aaff6e4dfa7bb
Jose Mario <z7design@z7design.com.br>
2021-04-25 04:14:00 -0300

    Project Created

---
+++ src/main/java/com/zemarioramos/viacepzup/ViacepzupApplication.java
@@ -0,0 +1,13 @@
+package com.zemarioramos.viacepzup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ViacepzupApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ViacepzupApplication.class, args);
+	}
+
+}
